0b2b78337c6345852c3e610e7d5076eb83e18994
William <William@MacBook-Pro-2.local>
2018-04-24 19:10:49 -0700

    added duplicate letter allert.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ var loss=0;
 var guessleft=9;
 var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 // assign an empty array
-
 var sofar =[];
 var computerGuess=alphabet[Math.floor(Math.random()*alphabet.length)];
 var random=Math.floor(Math.random()*alphabet.length);
@@ -12,21 +11,29 @@ var random=Math.floor(Math.random()*alphabet.length);
 // onkeyupfunction
 document.onkeyup=function(game) {
     var userInput=game.key;
+    // var strinput=string(userInput);
+    // console.log(stringinput);
 //  !!!!
-    if (computerGuess === userInput) {
-        wins++;
-        guessleft=9;
-        sofar=[];
-        computerGuess=alphabet[Math.floor(Math.random()*alphabet.length)];
+    if (sofar.indexOf(userInput) === -1){
+        document.getElementById("alert").innerHTML="keep going";
+        if (computerGuess === userInput) {
+            wins++;
+            guessleft=9;
+            sofar=[];
+            computerGuess=alphabet[Math.floor(Math.random()*alphabet.length)];
+        }
+        else if(guessleft > 1) {
+            guessleft=guessleft-1;
+            sofar.push(userInput);
+        }
+        else {
+            loss++;
+            guessleft=9;
+            sofar=[];
+        }
     }
-    else if(guessleft > 0) {
-        guessleft=guessleft-1;
-        sofar.push(userInput);
-    }
-    else {
-        loss++;
-        guessleft=9;
-        sofar=[];
+    else{
+        document.getElementById("alert").innerHTML="Duplicated letter, try again!";
     }
     document.getElementById("wins").innerHTML=wins;
     document.getElementById("loss").innerHTML=loss;
